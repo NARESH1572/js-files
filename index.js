c408d75ce5de43581e551a4974910a1248db9fe3
THOTLA NARESH <thotlanaresh2002@gmail.com>
2024-12-17 19:46:17 +0530

    day1

---
+++ index.js
@@ -0,0 +1,29 @@
+let h1Tag=document.getElementById("h1");
+console.log(h1Tag)
+h1Tag.style.backgroundColor="red"
+h1Tag.style.padding="60px"
+h1Tag.style.cursor="pointer"
+function changetext(){
+    h1Tag.innerText="vamsi is from 10kcoders"
+}
+
+let h2Tag=document.getElementById("h2");
+console.log(h2Tag)
+h2Tag.style.backgroundColor="red"
+h2Tag.style.padding="60px"
+h2Tag.style.cursor="pointer"
+function changetextondblclick(){
+    h2Tag.innerText="vamsi is from 10coders"
+    
+}
+
+let h3Tag=document.getElementById("h3")
+console.log(h3Tag)
+h3Tag.style.backgroundColor="red"
+h3Tag.style.padding="60px"
+h3Tag.style.cursor="pointer"
+h3Tag.style.boxShadow="2px 2px 5px black"
+h3Tag.style.borderRadius="100px"
+function changetextonmouseover(){
+    h3Tag.innerText="vamsi is from 10kcodees"
+}
